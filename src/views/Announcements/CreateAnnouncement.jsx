c975cbd6d240b8f7b46320363225b0c177d7ae56
Milosz Konopka <Milosz1532@gmail.com>
2023-07-17 20:59:07 +0200

    Add animations to createAnnouncement view

--- src/views/Announcements/CreateAnnouncement.jsx
+++ src/views/Announcements/CreateAnnouncement.jsx
@@ -1,9 +1,70 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../assets/styles/CreateAnnouncement.scss'
 
+const Step1 = () => {
+	return <p>Krok 1</p>
+}
+
+const Step2 = () => {
+	return <p>Krok 2</p>
+}
+
+const Step3 = () => {
+	return <p>Krok 3</p>
+}
+
+const Step4 = () => {
+	return <p>Krok 4</p>
+}
+
 const CreateAnnouncement = () => {
+	const [activeStep, setActiveStep] = useState(2)
+
+	const progressItems = [
+		{
+			title: 'Co chcesz sprzedać?',
+			subTitle: 'Tutaj decydujesz co chcesz sprzedać',
+			icon: 'fa-regular fa-file',
+		},
+		{
+			title: 'Galeria zdjęć',
+			subTitle: 'Tutaj możesz dodać zdjęcia i opis do swojego ogłoszenia',
+			icon: 'fa-solid fa-images',
+		},
+		{
+			title: 'Twoja wycena',
+			subTitle: 'To ty decydujesz ile chcesz zarobić na swoim przedmiocie',
+			icon: 'fa-solid fa-coins',
+		},
+		{
+			title: 'Podsumowanie',
+			subTitle: 'Podsumowanie',
+			icon: 'fa-regular fa-circle-check',
+		},
+	]
+
+	const getCurrentStepComponent = () => {
+		switch (activeStep) {
+			case 1:
+				return <Step1 />
+			case 2:
+				return <Step2 />
+			case 3:
+				return <Step3 />
+			case 4:
+				return <Step4 />
+			default:
+				return null
+		}
+	}
+
+	const handleChangeStep = step => {
+		setActiveStep(step)
+	}
+
 	return (
 		<>
 			<div className='container'>
@@ -20,84 +81,40 @@ const CreateAnnouncement = () => {
 						<div className='container-content'>
 							<h5 className='container-content-title'>Postęp</h5>
 							<div className='progress-container'>
-								<div className='progress-item completed'>
-									<div className='description'>
+								{progressItems.map((item, index) => (
+									<div
+										key={index}
+										className={`progress-item ${activeStep === index + 1 && 'active'} ${
+											activeStep > index + 1 && 'completed'
+										}`}>
 										<div className='description'>
-											<p className='title'>Co chcesz sprzedać?</p>
-											<p className='sub-title'>Tutaj decydujesz co chcesz sprzedać</p>
+											<p className='title'>{item.title}</p>
+											<p className='sub-title'>{item.subTitle}</p>
+										</div>
+										<div className='step-number' onClick={e => handleChangeStep(index + 1)}>
+											<i>
+												<FontAwesomeIcon icon={item.icon} />
+											</i>
 										</div>
 									</div>
-									<div className='step-number'>
-										<i>
-											<FontAwesomeIcon icon='fa-regular fa-file' />
-										</i>
-									</div>
-								</div>
-								<div className='progress-item active'>
-									<div className='description'>
-										<p className='title'>Galeria zdjęć</p>
-										<p className='sub-title'>
-											Tutaj możesz dodać zdjecia i opis do swojego ogłoszenia
-										</p>
-									</div>
-									<div className='step-number'>
-										<i>
-											<FontAwesomeIcon icon='fa-solid fa-images' />
-										</i>
-									</div>
-								</div>
-								<div className='progress-item'>
-									<div className='description'>
-										<p className='title'>Twoja wycena</p>
-										<p className='sub-title'>
-											To ty decydujesz ile chcesz zarobić na swoim przedmiocie
-										</p>
-									</div>
-									<div className='step-number'>
-										<i>
-											<FontAwesomeIcon icon='fa-solid fa-coins' />
-										</i>
-									</div>
-								</div>
-								<div className='progress-item'>
-									<div className='description'>Podsumowanie</div>
-									<div className='step-number'>
-										<i>
-											<FontAwesomeIcon icon='fa-regular fa-circle-check' />
-										</i>
-									</div>
-								</div>
+								))}
 							</div>
 						</div>
 					</div>
 					<div className='scrollable-container'>
 						<div className='container-content'>
-							<h5 className='container-content-title'>Krok 1: Przedmiot sprzedaży</h5>
-
-							<p>
-								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, excepturi?
-								Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia quidem,
-								saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum doloribus.
-								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, excepturi?
-								Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia quidem,
-								saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-								excepturi? Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia
-								quidem, saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-								excepturi? Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia
-								quidem, saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-								excepturi? Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia
-								quidem, saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-								excepturi? Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia
-								quidem, saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
-								excepturi? Adipisci deserunt, quibusdam possimus excepturi commodi laborum mollitia
-								quidem, saepe necessitatibus quos pariatur nulla aut dolorum modi tenetur nostrum
-								doloribus.
-							</p>
+							<h5 className='container-content-title'>
+								Krok {activeStep}: {progressItems[activeStep - 1].title}
+							</h5>
+							<motion.div
+								key={activeStep}
+								initial={{ opacity: 0, y: '-100%' }}
+								animate={{ opacity: 1, y: 0 }}
+								exit={{ opacity: 0, y: '100%' }}
+								transition={{ duration: 0.5 }}>
+								{getCurrentStepComponent()}{' '}
+								{/* Wyświetl odpowiedni komponent dla aktualnego kroku */}
+							</motion.div>
 						</div>
 					</div>
 				</div>
